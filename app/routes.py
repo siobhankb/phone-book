09fba3a5520842e6ee3deee2f1392c82523f09f8
siobhankb <siobhan.boylan@gmail.com>
2022-06-08 02:35:34 -0500

    working on flash message

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
 from app import app
-from flask import render_template, url_for, flash
+from flask import render_template, url_for, redirect, flash
 from app.forms import ContactForm
 
 @app.route('/home')
 def home():
     return render_template('home.html')
 
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = ContactForm()
+    if form.validate_on_submit():
+        flash('Contact successfully added!', 'success')
     return render_template('signup.html', form=form)
